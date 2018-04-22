37bda07f0637b80f2e55094303d666f664de58cd
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-04-22 00:18:34 -0500

    Made Forward and backward work in relation to the parents x position

--- Assets/PartyMembers/PartyMember.cs
+++ Assets/PartyMembers/PartyMember.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class PartyMember : MonoBehaviour {
 
     public PartyMemberAnimation animation;
+    int offset;
 
 	// Use this for initialization
 	void Start () {
+        offset = (int)transform.localPosition.x;
     }
 
     public void Attack()
@@ -19,12 +21,14 @@ public class PartyMember : MonoBehaviour {
 
     public void Forward()
     {
-        animation.Forward();
+        offset += 1;
+        animation.MoveToX((int)transform.parent.position.x + offset);
     }
 
     public void Backward()
     {
-        animation.Backward();
+        offset -= 1;
+        animation.MoveToX((int)transform.parent.position.x + offset);
     }
 
 }
