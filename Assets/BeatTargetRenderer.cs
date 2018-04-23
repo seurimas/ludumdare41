2fd8f38b9c30532f1806fa444f4fb25f08dab7b6
Seurimas <nicholasanderson5@gmail.com>
2018-04-22 22:22:06 -0500

    Change up the beat indicator.

---
+++ Assets/BeatTargetRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatTargetRenderer : MonoBehaviour, IRhythmListener {
+    public float maxScale = 1.4f;
+    private GameObject stable;
+    private GameObject pulse;
+    // Use this for initialization
+    void Start () {
+        stable = transform.Find("Stable").gameObject;
+        pulse = transform.Find("Pulse").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float progress = RhythmManager.instance.GetTTB() / RhythmManager.instance.beatTime;
+        float scale = Mathf.LerpUnclamped(1, maxScale, progress);
+        pulse.transform.localScale = new Vector3(scale, scale, 1);
+    }
+    public void OnBeatRight(int beatNumber)
+    {
+    }
+
+    public void OnFailure(List<Notes> failedRhythm, bool tooEarly)
+    {
+    }
+
+    public bool OnNote(Notes note, List<Notes> fullRhythm)
+    {
+        return false;
+    }
+}
