2fc9c4598e55bc179f4c33e5dc4c603f530940ef
Seurimas <nicholasanderson5@gmail.com>
2018-04-23 17:27:22 -0500

    Add game over scene transitions.

---
+++ Assets/GameOverController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.Find("Panel").Find("MainMenu").GetComponent<Button>().onClick.AddListener(MainMenuClick);
+        transform.Find("Panel").Find("Replay").GetComponent<Button>().onClick.AddListener(ReplayClick);
+    }
+
+    public void MainMenuClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ReplayClick()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
