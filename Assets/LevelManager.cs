ff3ff87a020bbf0bd8395f07c8e011da2525cd58
Alvaro Sanchez Diaz <heyalvaro@gmail.com>
2018-04-23 18:30:29 -0500

    Menus

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
